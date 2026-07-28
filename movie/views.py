a18dc1a29d1f2974b9d69105634544a8b549c456
scanonc <scanonc@eafit.edu.co>
2026-07-28 10:59:27 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {
-        'name': 'Sebastian Cañon'
+        'name': 'Sebastian Cañon cuartas'
     })
 
 def about(request):
