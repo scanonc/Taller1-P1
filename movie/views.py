fd316ffa45519e9b7e1fb54c7589f3015c4e444c
scanonc <scanonc@eafit.edu.co>
2026-07-28 14:18:06 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+        
     return render(request, 'home.html', {
-        'name': 'Sebastian Cañon cuartas'
+        'searchTerm': searchTerm, 
+        'movies': movies
     })
 
 def about(request):
-    return HttpResponse("About")
+    return render(request, 'about.html')
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as MovieViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( '' , MovieViews.home),
     path( 'about/' , MovieViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
